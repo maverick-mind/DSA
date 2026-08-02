47486ed9f8227e5194d4a9e4a9ffee0f729a741b
Ankit <75ankitraj@gmail.com>
2026-08-02 19:01:12 +0530

    Min Heap STL

--- 41-Heap/003Heaps_STLMinHeap.cpp
+++ 41-Heap/003Heaps_STLMinHeap.cpp
@@ -21,6 +21,8 @@ int main() {
 
 	priority_queue<int /* datatype of the heap */, deque<int> /* internal representation of the heap */, greater<int>/* comparator needed for sorting */> m;
 
+    // internally , greater<int> is a class. SO if we write our custom comparator , that should be inside a class , and should be a functor 
+
 	m.push(9);
 	m.push(7);
 	m.push(8);
