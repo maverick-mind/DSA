1ed86b485dff0b8bb62aa9f0454ef223886dabe7
Ankit <75ankitraj@gmail.com>
2026-09-26 15:01:49 +0530

    Left shift and Right Shift

--- 07-Bitwise_operator/003BitwiseShiftOperators.cpp
+++ 07-Bitwise_operator/003BitwiseShiftOperators.cpp
@@ -6,12 +6,15 @@ int main() {
 
 	int a = 1;
 
+	// left shift to k places means : multiplying the inital number with 2^k
+
 	cout << "a << 1 = " << (a << 1) << endl;
 	cout << "a << 2 = " << (a << 2) << endl;
 	cout << "a << 3 = " << (a << 3) << endl << endl;
 
 	a = 8;
 
+	// right shift to k places means : dividing the initial number by 2^k 
 	cout << "a >> 1 = " << (a >> 1) << endl;
 	cout << "a >> 2 = " << (a >> 2) << endl;
 	cout << "a >> 3 = " << (a >> 3) << endl << endl;
